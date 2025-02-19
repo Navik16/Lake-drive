8f0cbb9c3bb099c6a8c3ff08f66f9aa112d379f8
Navik16 <navikbandara16@gmail.com>
2025-02-19 00:28:29 +0000

    Update sys.js

--- sys.js
+++ sys.js
@@ -134,11 +134,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
